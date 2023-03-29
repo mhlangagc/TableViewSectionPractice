5b7c19749910df34c7ac192ddcc427e20b45329c
Gugulethu <mhlangagc@gmail.com>
2023-03-29 15:26:29 +0200

    Initial Commit

---
+++ TableViewSectionPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewSectionPractice
+//
+//  Created by Gugulethu Mhlanga on 2023/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
